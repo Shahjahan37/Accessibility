e468161e9b2b85bc03af728a77badf4df6e8028f
Shahjahan Ali <shahjahanalishaikh21@gmail.com>
2025-08-31 19:41:00 +0530

    created modal

--- trap-focus/a11y.js
+++ trap-focus/a11y.js
@@ -1,1 +1,22 @@
 console.log("focus trap");
+
+function createPopup(id) {
+    let popupNode = document.querySelector(id);
+    let overlay = popupNode.querySelector(".overlay");
+    let closeBtn = popupNode.querySelector(".close-btn");
+
+    function openPopup() {
+        popupNode.classList.add("active");
+    }
+
+    function closePopup() {
+        popupNode.classList.remove("active");
+    }
+
+    overlay.addEventListener("click", closePopup);
+    closeBtn.addEventListener("click", closePopup);
+    return openPopup;
+}
+
+let popup = createPopup("#popup");
+document.getElementById("open-popup").addEventListener("click", popup);
